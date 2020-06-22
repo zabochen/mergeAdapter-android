ae86a75e4c6b786e60959f941079a392b39800ee
TarasZabochen <zabochen@gmail.com>
2020-06-22 22:57:15 +0300

    Init RecyclerView with MergeAdapter

--- app/src/main/java/ua/ck/zabochen/android/mergeadapter/ui/main/MainActivity.kt
+++ app/src/main/java/ua/ck/zabochen/android/mergeadapter/ui/main/MainActivity.kt
@@ -1,12 +1,35 @@
 package ua.ck.zabochen.android.mergeadapter.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.MergeAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import ua.ck.zabochen.android.mergeadapter.R
+import ua.ck.zabochen.android.mergeadapter.model.User
+import ua.ck.zabochen.android.mergeadapter.ui.main.adapter.FooterAdapter
+import ua.ck.zabochen.android.mergeadapter.ui.main.adapter.HeaderAdapter
+import ua.ck.zabochen.android.mergeadapter.ui.main.adapter.UserListAdapter
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() = with(rvUserList) {
+        layoutManager = LinearLayoutManager(this@MainActivity)
+
+        // Adapters
+        val headerAdapter = HeaderAdapter()
+        val userAdapter = UserListAdapter().apply { setData(userList = getUserList()) }
+        val footerAdapter = FooterAdapter()
+        adapter = MergeAdapter(headerAdapter, userAdapter, footerAdapter)
+    }
+
+    private fun getUserList() = MutableList(100) {
+        User(it, "User_$it")
     }
 }
