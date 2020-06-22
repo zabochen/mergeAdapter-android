8eb13b1edcb2e7782239a1c5280ffa0fb077823a
TarasZabochen <zabochen@gmail.com>
2020-06-22 21:25:09 +0300

    Init project

---
+++ app/src/main/java/ua/ck/zabochen/android/mergeadapter/MainApp.kt
@@ -0,0 +1,5 @@
+package ua.ck.zabochen.android.mergeadapter
+
+import android.app.Application
+
+class MainApp : Application()
---
+++ app/src/main/java/ua/ck/zabochen/android/mergeadapter/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package ua.ck.zabochen.android.mergeadapter.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ua.ck.zabochen.android.mergeadapter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
